e50e15cede358d1b111fb3730f90420af3b902c0
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-03-20 10:30:10 +0100

    App Search Connection manager.

---
+++ Client/ClientConfiguration.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * This file is part of the App Search Magento module.
+ *
+ * (c) Elastic
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Elastic\AppSearch\Client;
+
+/**
+ * Implementation of the App Search client configuration.
+ *
+ * @package   Elastic\AppSearch\Client
+ * @copyright 2019 Elastic
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+class ClientConfiguration implements ClientConfigurationInterface
+{
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var \Magento\Framework\Encryption\EncryptorInterface
+     */
+    private $encryptor;
+
+    /**
+     * Constructor.
+     *
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\Encryption\EncryptorInterface   $encryptor
+     */
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\Encryption\EncryptorInterface $encryptor
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->encryptor = $encryptor;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getApiEndpoint()
+    {
+        return (string) $this->scopeConfig->getValue('elastic_appsearch/client/api_endpoint');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getApiKey()
+    {
+        $encryptedApiKey = $this->scopeConfig->getValue('elastic_appsearch/client/api_key');
+
+        return (string) $this->encryptor->decrypt($encryptedApiKey);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDebug()
+    {
+        return (string) $this->scopeConfig->getValue('elastic_appsearch/client/is_debug');
+    }
+}
---
+++ Client/ConnectionManager.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * This file is part of the App Search Magento module.
+ *
+ * (c) Elastic
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Elastic\AppSearch\Client;
+
+use Psr\Log\LoggerInterface;
+use Swiftype\AppSearch\ClientBuilder;
+
+/**
+ * Retrieve a configured and ready to go App Search client.
+ *
+ * @api
+ *
+ * @package   Elastic\AppSearch\Client
+ * @copyright 2019 Elastic
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+class ConnectionManager
+{
+    /**
+     * @var \Swiftype\AppSearch\Client
+     */
+    private $client;
+
+    /**
+     * @var ClientConfigurationInterface
+     */
+    private $clientConfig;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Constructor.
+     *
+     * @param ClientConfigurationInterface $clientConfig
+     * @param LoggerInterface              $logger
+     */
+    public function __construct(ClientConfigurationInterface $clientConfig, LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+        $this->clientConfig = $clientConfig;
+    }
+
+    /**
+     * Retrieve the configured App Search client.
+     *
+     * @param array $options
+     *
+     * @return \Swiftype\AppSearch\Client
+     */
+    public function getClient($options = [])
+    {
+        if (null === $this->client) {
+            $this->client = $this->createClient($options);
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * Create the App Search client.
+     *
+     * @param array $options
+     *
+     * @return \Swiftype\AppSearch\Client
+     */
+    private function createClient($options = [])
+    {
+        $apiEndpoint = $options['api_endpoint'] ?? $this->clientConfig->getApiEndpoint();
+        $apiKey = $options['api_key'] ?? $this->clientConfig->getApiKey();
+
+        $clientBuilder = ClientBuilder::create($apiEndpoint, $apiKey);
+
+        $clientBuilder->setLogger($this->logger);
+
+        if ($this->clientConfig->isDebug()) {
+            $clientBuilder->setTracer($this->logger);
+        }
+
+        return $clientBuilder->build();
+    }
+}
