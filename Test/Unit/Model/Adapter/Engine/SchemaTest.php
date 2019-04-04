b6b7af5a532d567029813c97aba71450ead4c5bd
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-04-04 13:49:21 +0200

    AppSearch schema models.

---
+++ Test/Unit/Model/Adapter/Engine/SchemaTest.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the App Search Magento module.
+ *
+ * (c) Elastic
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Elastic\AppSearch\Test\Unit\Model\Adapter\Engine;
+
+use Elastic\AppSearch\Model\Adapter\Engine\Schema;
+
+/**
+ * Unit test for the Elastic\AppSearch\Model\Adapter\Engine\Schema class.
+ *
+ * @package   Elastic\AppSearch\Test\Unit\Client
+ * @copyright 2019 Elastic
+ * @license   Open Software License ("OSL") v. 3.0
+ */
+class SchemaTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test expected language mapping.
+     *
+     * @param array $fields
+     *
+     * @testWith [{"foo": "text", "bar": "number"}]
+     */
+    public function testCreateSchema($fields)
+    {
+        $schema = new Schema($fields);
+
+        $this->assertEquals($fields, $schema->getFields());
+    }
+}
